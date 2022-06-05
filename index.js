481cf0507a6e31b73b544dd7cf3828859364bbf8
jangismarifet <jangismarifet@gmail.com>
2022-06-05 22:13:33 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Write your code here!
+// const element = document.createElement("div");
+// document.body.append(element);
+// const ul = document.createElement("ul");
+
+// for (let i = 0; i < 3; i++) {
+//   const li = document.createElement("li");
+//   li.textContent = (i + 1).toString();
+//   ul.append(li);
+// }
+
+// element.append(ul);
+// element.textContent = "You've changed what's on the screen!";
+// element.style.fontSize = "24px";
+// element.style.marginLeft = "30px";
+// element.style.lineHeight = 2;
+
+const main = document.getElementById("main");
+main.remove();
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.textContent = "YOUR-NAME is the champion";
+document.body.append(newHeader);
+
